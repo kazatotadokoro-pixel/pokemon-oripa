4ee5b34199371c8bf4b8b20d29a4de6a194c4b4d
kazatotadokoro-pixel <kazato.tadokoro@gmail.com>
2026-05-23 23:58:19 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect, useRef, useMemo } from "react";
+export default function App(){
+  return <div style={{color:"#fff",background:"#06060e",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}}>
+    <h1>ポケモンオリパ 🎴</h1>
+  </div>
+}
